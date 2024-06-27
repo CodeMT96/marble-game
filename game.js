760a3071e220121494290fd4d956320eb9077558
markus <markusteumel@gmail.com>
2024-06-27 10:55:46 +0200

    added physics to plane and box

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
-import * as CANNON from "cannon-es"
+import * as CANNON from "cannon-es";
+import { CannonCombiner } from "./libary/CannonCombiner";
+import {OrbitControls} from "./node_modules/three/examples/jsm/controls/OrbitControls"
 
 //Init three.js basics
 //Defining the Game via Class
@@ -31,36 +33,109 @@ class Game {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     //appending the renderer to the container
     gameContainer.appendChild(this.renderer.domElement);
-    //Eventlistener for resize 
-    window.addEventListener('resize', this.resize.bind(this))
+    //Eventlistener for resize
+    window.addEventListener("resize", this.resize.bind(this));
     //Loop to see changes and keep rendering
-    this.renderer.setAnimationLoop(this.render.bind(this))
+    this.renderer.setAnimationLoop(this.render.bind(this));
     //Lights
-    this.globalLight = new THREE.HemisphereLight(0xfffff,5)
+    this.globalLight = new THREE.HemisphereLight(0xfffff, 5);
     //Positioning the light
-    this.globalLight.position.set(0.2, 1, 1)
+    this.globalLight.position.set(0.2, 1, 1);
     //adding the light to the scene
-    this.scene.add(this.globalLight)
+    this.scene.add(this.globalLight);
     //A direct Light
-    const directLight = new THREE.DirectionalLight(0xfffff,10)
+    const directLight = new THREE.DirectionalLight(0xfffff, 10);
     //Direct light Postion
-    directLight.position.set(0.2, 1 ,1)
+    directLight.position.set(0.2, 1, 1);
     //Direct light Rotation
     //Nothing here yet
     //Adding the direct light into the scene
-    this.scene.add(directLight)
+    this.scene.add(directLight);
+    //Orbit Controls
+    const controls = new OrbitControls( this.camera, this.renderer.domElement )
     //TestCube Geometry
-    const blockGeometry = new THREE.BoxGeometry()
+    const blockGeometry = new THREE.BoxGeometry();
     //TestCube Material
-    const blockMaterial = new THREE.MeshStandardMaterial({color:0x1242})
+    const blockMaterial = new THREE.MeshStandardMaterial({ color: 0x1242 });
     //Combining material and geometry into a full mesh
-    this.block = new THREE.Mesh(blockGeometry, blockMaterial)
+    this.block = new THREE.Mesh(blockGeometry, blockMaterial);
     //Positioning the Block
     console.log(this.block);
-    this.block.position.set(0,0,0)
+    this.block.position.set(0, 4, 0);
+
     //Adding the block
-    this.scene.add(this.block)
+    this.scene.add(this.block);
+    this.planeMaker();
+    this.initWorld();
+    this.initBasePlane();
+    this.initBlock();
+    this.animate();
   }
+  initWorld() {
+    const world = new CANNON.World();
+    world.gravity.set(0, -10, 0);
+
+    this.world = world;
+  }
+
+  //Adding a Plane
+  planeMaker() {
+    const planeGeo = new THREE.PlaneGeometry();
+    const planeMat = new THREE.MeshStandardMaterial({
+      color: 0x44456,
+    });
+    this.planeObj = new THREE.Mesh(planeGeo, planeMat);
+    this.planeObj.rotation.x = -Math.PI / 2;
+    this.planeObj.scale.set(5, 5, 5);
+    this.scene.add(this.planeObj);
+    console.log(this.planeObj);
+  }
+  initBlock() {
+    const blockShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));//WARUM DIE HÄLFTE!!!!
+    //CHATGPT fragen später
+
+    const blockBody = new CANNON.Body({
+      mass: 2,
+      position: new CANNON.Vec3(
+        this.block.position.x,
+        this.block.position.y,
+        this.block.position.z
+      ),
+      shape: blockShape,
+    });
+    this.blockBody = blockBody;
+    this.world.addBody(blockBody);
+  }
+
+  initBasePlane() {
+    const planeShape = new CANNON.Plane(new CANNON.Vec3(1, 1, 1));
+    const basePlaneBody = new CANNON.Body({
+      mass: 0,
+      position: new CANNON.Vec3(
+        this.planeObj.position.x,
+        this.planeObj.position.y,
+        this.planeObj.position.z
+      ),
+      shape: new CANNON.Plane(),
+    });
+    basePlaneBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    this.basePlaneBody = basePlaneBody
+    this.world.addBody(basePlaneBody);
+  }
+
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.world.fixedStep();
+
+    this.block.position.copy(this.blockBody.position);
+    this.block.quaternion.copy(this.blockBody.quaternion);
+
+    this.basePlaneBody.position.copy(this.planeObj.position);
+    this.basePlaneBody.quaternion.copy(this.planeObj.quaternion);
+
+    this.renderer.render(this.scene, this.camera, this.world);
+  };
+
   //Method to Resize the window
   resize() {
     this.camera.aspect = window.innerWidth / window.innerHeight;
@@ -69,8 +144,8 @@ class Game {
   }
   //Render Method
   render() {
-    this.renderer.render(this.scene, this.camera,);
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
-export {Game}
+export { Game };
---
+++ libary/CannonCombiner.js
@@ -0,0 +1,12 @@
+import * as THREE from "three"
+import * as CANNON from "cannon-es"
+
+class CannonCombiner{
+    constructor(scene, world){
+        this.scene = scene
+        this.world = world
+    }
+}
+
+
+export { CannonCombiner }
